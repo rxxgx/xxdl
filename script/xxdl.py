3ea093b644505d288e9c578550e8e7ad54040230
rxxgx <105639747+rxxgx@users.noreply.github.com>
2022-05-25 17:04:16 +0700

    Update xxdl.py

--- script/xxdl.py
+++ script/xxdl.py
@@ -1,5 +1,5 @@
 import os
 url = os.getenv("URL")
-args = '--threads 8 --wait  -o "DL/(%(channel_id)s)_[%(date)s]_%(channel_name)s_%(title)s.%(ext)s" ' + url
+args = '--threads 8 --wait  -o "DL/%(channel_name)s(%(channel_id)s) [%(date)s] %(title)s.%(ext)s" ' + url
 download_command = 'sudo fc2-live-dl -x --wait-for-quality-timeout 15 %s ' % (args)
 os.system(download_command)
