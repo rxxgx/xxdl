921859171e6e949baaf3b9d6ad9b8cded85283d3
rxxgx <105639747+rxxgx@users.noreply.github.com>
2022-05-25 13:48:27 +0700

    Update xxdl.py

--- xxdl.py
+++ xxdl.py
@@ -1,3 +1,13 @@
 import os
 
-os.system(os.getenv("CLI"))
+url = os.getenv("URL")
+audio_only = os.getenv("AUDIO_ONLY")
+
+args = '--threads 8 --wait  -o "output/(%(channel_id)s)_[%(date)s]_%(channel_name)s_%(title)s.%(ext)s" ' + url
+
+if audio_only == 'true':
+  download_command = 'sudo fc2-live-dl --quality sound %s ' % (args)
+else:
+  download_command = 'sudo fc2-live-dl -x --wait-for-quality-timeout 15 %s ' % (args)
+
+os.system(download_command)
